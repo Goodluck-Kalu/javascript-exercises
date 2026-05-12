442d1979d50afd3e624d1f93e1d9761b56f2bfb1
Goodluck <182907427+Goodluck-Kalu@users.noreply.github.com>
2026-05-12 17:34:28 +0100

    check maths

---
+++ foundations/04_mathEquations/dist/mathEquations.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var a = 1 - 1; // Freebie!!! This is the answer to "one minus one"
+
+var b = 1 + 8;
+var c = 22 * 3;
+var d = 5 % 4;
+var e = b - 17;
+var f = a + b + c + d + e; // Do not edit below this line
+
+module.exports = {
+  a: a,
+  b: b,
+  c: c,
+  d: d,
+  e: e,
+  f: f
+};
